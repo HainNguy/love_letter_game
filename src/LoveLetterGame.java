41225eca87d7a9691f350a25c98742b01b0e6273
bernstein <ngochai.nguyen.phys@gmail.com>
2023-10-20 23:41:28 +0200

    Edited classes LoveLetterGame and Player

--- src/LoveLetterGame.java
+++ src/LoveLetterGame.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class LoveLetterGame {
@@ -16,22 +17,65 @@ public class LoveLetterGame {
         Scanner scanner = new Scanner(System.in);
         int playerCount = 0;
         while(playerCount < 2 || playerCount > 4){
-            System.out.println("Please enter the number of players (from 2 to 4) : ");
+            System.out.print("Please enter the number of players (from 2 to 4) : ");
             playerCount = scanner.nextInt();
         }
+        System.out.println("\nNow you are going to enter your names. " +
+                "Please use different player names. If some of you have" +
+                "the same names, please indicate with a number after your name," +
+                " for example: Alice1 and Alice2. ");
+
         for (int i = 0; i < playerCount; i++) {
             System.out.print("Name for player " + (i+1) + ":");
             String name = scanner.next();
             players.add(new Player(name));
         }
     }
-    void welcome(){}
-    void showCommands(){}
+    void welcome(){
+        System.out.println(
+                "Wellcome to Love Letter." +
+                        "Make sure that you have already read the game rules before playing the game."+
+        "To see the game's commands with a short explanation, enter \\help" +
+                "or enter \\start to start the game");
+    }
+    void showCommands(){
+        System.out.println("\nAvailable commands:");
+        System.out.println("\\playCard - Discard a card ");
+        System.out.println("\\showHand - show your hand");
+        System.out.println("\\showScore - show your score");
+    }
+
     void showHand(){}
     void showScore(){}
     void playCard(){}
-    void startGame(){}
+    void startGame(){
+        System.out.println("Welcome to Love Letter. Please make sure that you have " +
+                "already read the game rules before playing the game" );
+        getNumberOfPlayers();
+        Collections.shuffle(deckOfCards);
+        dealInitialCards();
 
+    }
+    void dealInitialCards() {
+        //If there are 2 players, 4 cards will be removed from deck.
+        if (players.size() == 2) {
+            for (int i = 0; i < 4; i++) {
+                deckOfCards.remove(i);
+            }}
+        else {
+            deckOfCards.remove(0);
+        }
+        for (Player player : players){
+            for (int i = 0; i < 1; i++) {
+                player.drawCard(deckOfCards.get(0), deckOfCards);
+            }
+        }
+
+
+    }
+    void playGame(){
+        System.out.println("playing");
+    }
     public int getTokenToWin() {
         return tokenToWin;
     }
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,38 @@
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+    private String name;
+    ArrayList<Card> hand = new ArrayList<>();
+    private int score;
+    private int token;
+    ArrayList<Card> discardPile = new ArrayList<>();
+
+    public Player(String name){
+        this.name = name;
+        this.score = 0;
+        this.token = 0;
+    }
+
+    public void drawCard(Card card, ArrayList<Card> deckOfCards){
+        hand.add(card);
+        deckOfCards.remove(card);
+    }
+
+    public void playCard(){}
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getToken() {
+        return token;
+    }
+
+    public void setToken(int token) {
+        this.token = token;
+    }
 }
