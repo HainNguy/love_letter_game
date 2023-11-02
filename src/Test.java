3f67e61631d3f31ee38a69e45ae93bfda1181ca3
bernstein <ngochai.nguyen.phys@gmail.com>
2023-11-02 22:49:32 +0100

    Delete test classes for whoPlayFirst

--- src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        LoveLetterGame game = new LoveLetterGame();
-        game.startGame();
-        System.out.println("The first Player is: " + game.players.get(game.getCurrentPlayerIndex()));
-
-    }
-}
